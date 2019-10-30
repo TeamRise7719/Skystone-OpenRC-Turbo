88cf6ebabe6ac4d0b48be75e31ea47675d09b7e6
srcardosi <srcardosi@icloud.com>
2019-10-30 14:37:23 -0400

    Removed some TODO statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/BasicTeleOp.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.Subsystems.Driving.Drivetrain;
  * Created by Sean Cardosi on 9/22/2019.
  */
 @TeleOp(name = "BasicTeleOp", group = "Drive")
-public class BasicTeleOp extends OpMode {//TODO: We need a robot naming convention... we just need a robot name.
+public class BasicTeleOp extends OpMode {
 
     private Drivetrain robot;
 
@@ -55,7 +55,6 @@ public class BasicTeleOp extends OpMode {//TODO: We need a robot naming conventi
         //----------------------------------------------=+(Drivetrain)+=----------------------------------------------\\
 
 
-        //TODO: This will need to be double checked. Unsure of the correct trigger values. Check motor direction.
         //----------------------------------------------=+(Intake)+=----------------------------------------------\\
         if ((gamepad1.left_trigger > 0.1)&&(Math.abs(gamepad1.right_trigger ) < 0.1)) {//This is a precaution
             component.intakeStone();//Intake stone
