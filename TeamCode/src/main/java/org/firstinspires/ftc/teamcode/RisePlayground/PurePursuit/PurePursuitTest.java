a4a48b34b0cfbbf07aacc24a8dc79d7e204aa1ff
jjnutty <44911827+jjnutty@users.noreply.github.com>
2019-11-08 21:05:37 -0500

    Catch-Up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RisePlayground/PurePursuit/PurePursuitTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.RisePlayground.PurePursuit;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import java.util.ArrayList;
+
+import static org.firstinspires.ftc.teamcode.RisePlayground.PurePursuit.PurePursuitMovement.followCurve;
+
+/**
+ * Created by Sean Cardosi.
+ * Example of how to use Pure Pursuit to create an autonomous.
+ */
+@Autonomous(name = "Pure Pursuit Test", group = "Pure Pursuit")
+public class PurePursuitTest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        ArrayList<CurvePoint> allPoints = new ArrayList<>();
+        allPoints.add(new CurvePoint(0,0,1.0,1.0,50,Math.toRadians(50),1.0));
+
+        followCurve(allPoints, Math.toRadians(90));//Robot will get stuck spinning while looking for another point to go to at the endPoint.
+    }
+}
