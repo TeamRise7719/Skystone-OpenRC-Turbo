22456ce3adbb30910f34810ed0552f48984ea2af
srcardosi <srcardosi@icloud.com>
2019-11-12 13:20:43 -0500

    We in are the endgame now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeansSpace/Subsystems/Memes/RobotMedia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeansSpace/Subsystems/Memes/RobotMedia.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * @Author Sean Cardosi
@@ -11,14 +12,21 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  */
 public class RobotMedia {
 
+    ElapsedTime endgameTimer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
     MediaPlayer horn;
     MediaPlayer myaah;
+    MediaPlayer endgame;
     private boolean startState = false;
     private boolean backState = false;
+    private boolean endgamePlayed = false;
 
     public RobotMedia(HardwareMap hardwareMap) {
         horn = MediaPlayer.create(hardwareMap.appContext,com.qualcomm.ftcrobotcontroller.R.raw.horn);
         myaah = MediaPlayer.create(hardwareMap.appContext,com.qualcomm.ftcrobotcontroller.R.raw.myaah);
+        endgame = MediaPlayer.create(hardwareMap.appContext,com.qualcomm.ftcrobotcontroller.R.raw.endgame);
+    }
+    public void startTimer() {
+        endgameTimer.reset();
     }
 
     public void playSounds(Gamepad gamepad, HardwareMap hardwareMap) {
@@ -49,5 +57,13 @@ public class RobotMedia {
             myaah.stop();
         }
         //----------------------------------------------=+(Myaah)+=----------------------------------------------\\
+
+
+        //----------------------------------------------=+(Endgame)+=----------------------------------------------\\
+        if (endgameTimer.seconds() >= 90 && endgamePlayed == false) {
+            endgame.start();//We in da endgame now
+            endgamePlayed = true;
+        }
+        //----------------------------------------------=+(Endgame)+=----------------------------------------------\\
     }
 }
