df257b3a85a90fd86c8eb926c5519f7a15378212
srcardosi <srcardosi@icloud.com>
2019-11-30 15:39:09 -0500

    Fraser is here

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fraser/Autonomous/Blue_Auto_Depot_V2.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Fraser.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.Driving.RobotComponents;
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.Driving.SeansEncLibrary;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.Transitioning.AutoTransitioner;
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.VisionTargeting.SkystoneDetectionPhone;
+
+@Autonomous(name = "Blue Depot",group = "Fraser Blue Auto" )
+public class Blue_Auto_Depot_V2 extends LinearOpMode{
+
+    SeansEncLibrary enc;
+    SkystoneDetectionPhone cam;
+    RobotComponents mech;
+
+    public boolean isSkystone;
+
+    public void runOpMode() throws InterruptedException {
+
+        enc = new SeansEncLibrary(hardwareMap, telemetry, this);
+        mech = new RobotComponents(hardwareMap, telemetry);
+        cam = new SkystoneDetectionPhone(hardwareMap, telemetry);
+
+        enc.init();
+        mech.init();
+        cam.init();
+
+        while (true){
+
+            isSkystone = cam.TFdetect(isSkystone);
+
+            if(isStarted()){
+                break;
+            }
+
+        }
+
+        enc.steeringDrive(6, true,false);
+
+        enc.gyroTurn(enc.TURN_SPEED,-90);
+
+        enc.steeringDrive(12, true, false);
+
+        enc.gyroTurn(enc.TURN_SPEED,0);
+
+        enc.steeringDrive(38, true, false);
+
+        enc.steeringDrive(-6,true,false);
+
+        enc.gyroTurn(enc.TURN_SPEED, 90);
+
+        AutoTransitioner.transitionOnStop(this,"FraserTeleOp");
+
+    }
+}
