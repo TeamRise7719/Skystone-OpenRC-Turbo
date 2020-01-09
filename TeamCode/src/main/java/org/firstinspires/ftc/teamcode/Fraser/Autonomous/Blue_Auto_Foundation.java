b530724e64ced3b2c1e7d355663ebda645e9c329
jjnutty <44911827+jjnutty@users.noreply.github.com>
2020-01-09 16:58:43 -0500

    Autos for Fraser - Jordan

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fraser/Autonomous/Blue_Auto_Foundation.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.Fraser.Autonomous;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.Drive.FraserComponents;
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.Drive.SeansEncLibrary;
+import org.firstinspires.ftc.teamcode.Fraser.Subsystems.Transitioning.AutoTransitioner;
+
+
+@Autonomous (name = "Blue Auto Foundation", group = "Fraser Blue Auto")
+public class Blue_Auto_Foundation extends LinearOpMode{
+
+    ElapsedTime etime = new ElapsedTime();
+
+    public void waitFor(int time){
+        time = time/1000;
+        etime.reset();
+        while ((etime.time() < time)&&(opModeIsActive())) {
+            idle();
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //int position;
+        SeansEncLibrary enc = new SeansEncLibrary(hardwareMap, telemetry, this);
+        FraserComponents bacon = new FraserComponents(hardwareMap);
+
+        enc.init();
+
+        waitForStart();
+
+        enc.steeringDrive(-20, false,false);
+        enc.steeringDrive(-6.5, false,false);
+        enc.steeringDrive(11.5, false, true);
+        bacon.foundationRelease();
+        waitFor(2000);
+        enc.steeringDrive(28,false,false);
+        bacon.foundationGrab();
+        enc.steeringDrive(-30,false,true);
+
+        enc.steeringDrive(-22,false,false);
+        enc.gyroTurn(enc.TURN_SPEED,-90);
+        enc.steeringDrive(24, false, false);
+
+
+
+        AutoTransitioner.transitionOnStop(this,"MkITeleOp");
+    }
+}
+
+
